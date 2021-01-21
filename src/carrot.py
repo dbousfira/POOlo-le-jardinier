259cca293130bb266c96624b27b7338ff438d5a1
Djamel Bousfira <d.bousfira@hotmail.fr>
2021-01-21 09:23:44 +0100

    Final commit

--- src/carrot.py
+++ src/carrot.py
@@ -1,3 +1,4 @@
+from os import O_NOINHERIT
 from vegetable import Vegetable
 
 
--- src/vegetable.py
+++ src/vegetable.py
@@ -5,6 +5,5 @@ class Vegetable(ABC):
     def __init__(self) -> None:
         self.number = 0
 
-    @abstractmethod
     def grow(self, number=0):
-        pass
+        raise NotImplementedError
